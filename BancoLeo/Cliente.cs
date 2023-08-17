6e81d78200df678bd9efe009e79a68b3f19aac33
LeonardoGMendoza <desenvolvimento3000@outlook.com>
2023-08-17 00:07:10 -0300

    ok

---
+++ BancoLeo/Cliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoLeo
+{
+    public class Cliente
+    {
+        public string Codigo { get; private set; }
+        public string Nome { get; private set; }
+
+        public string Saldo { get; private set; }
+
+        public List<Movimentacao> Movimentacoes { get; set; }
+
+        public Cliente()
+        {
+            Movimentacoes = new List<Movimentacao>();
+        }
+        public Cliente(string codigo, string nome)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+        public void RealizarSaque(decimal valor)
+        {
+            if (Saldo > valor)
+            {
+                decimal valorMenosTaxa = DescontarTaxa(valor);
+                Saldo = Saldo - valor;
+                AdicionarMovimentacao("SAQUE", valorMenosTaxa);
+                Console.WriteLine($"Saque realizado com suceso Saldo: {Saldo}");
+
+            }
+            else
+                Console.WriteLine("Valor insuficiente");
+        }
+
+        public void RealizarDeposito(decimal valor)
+        {
+            if (valor >= 10)
+            {
+                decimal valorMenosTaxa = DescontarTaxa(valor);
+                Saldo += valorMenosTaxa;
+                AdicionarMovimentacao("DEPOSITO", valorMenosTaxa);
+                Console.WriteLine($"Deposito realizado com suceso Saldo: {Saldo}");
+
+            }
+            else
+                Console.WriteLine("Valor deve ser igual ou maior que R$ 10,00");
+
+        }
+        private void AdicionarMovimentacao(string tipo, decimal valor)
+        {
+            Movimentacoes.Add(new Movimentacao(tipo, DescontarTaxa(valor)));   
+        }
+        public virtual decimal DescontarTaxa(decimal valor)
+        {
+            return valor;
+        }
+    }
+}
---
+++ BancoLeo/Program.cs
@@ -0,0 +1,121 @@
+﻿using BancoLeo;
+
+List<Cliente> Clientes = new List<Cliente>();
+ConsultarCliente();
+
+
+void ConsultarCliente()
+{
+    Console.WriteLine("Ola benvindo ao Banco Leo");
+    Console.WriteLine("Digite seu codigo");
+    string codigo = Console.ReadLine();
+    Cliente cliente = null;
+
+    foreach (Cliente cli in Clientes)
+    {
+        if (cliente == null)
+        {
+            Console.WriteLine("Este cliente não existe.Deseja cadastrar?Digite S ou N");
+            string cadastrarCliente = Console.ReadLine();
+            if (cadastrarCliente == "S")
+            {
+                Console.WriteLine("Digite seu codigo:");
+                string CodigoClienteNovo = Console.ReadLine();
+                Console.WriteLine("Digite seu Nome:");
+                string NomeClienteNovo = Console.ReadLine();
+                Console.WriteLine("Digite PF ou PJ");
+                string tipoPessoa = Console.ReadLine();
+                if (tipoPessoa == "PF")
+                    cliente = new PessoaFisica(CodigoClienteNovo, NomeClienteNovo);
+                else
+                    cliente = new PessoaJuridica(CodigoClienteNovo, NomeClienteNovo);
+                Clientes.Add(cliente);
+                ExibirMenu(cliente);
+            }
+            else
+                ConsultarCliente();
+
+
+        }
+
+    }
+
+    void ExibirMenu(Cliente cliente)
+    {
+        Console.WriteLine($"Ola{cliente.Nome}");
+        Console.WriteLine("Digite a opcao desejada:");
+        Console.WriteLine("1 -Extrato");
+        Console.WriteLine("2 -Saque");
+        Console.WriteLine("3 -Deposito");
+
+        string menu = Console.ReadLine();
+
+        switch(menu)
+        {
+            case "1":
+                ExibirExtrato(cliente); 
+                break;
+            case "2":
+                RealizarSaque(cliente);
+                break;
+            case "3":
+                RealizarDeposito(cliente);
+                break;
+            default:
+                Console.WriteLine("Digite a opcao correta");
+                ExibirMenu(cliente);
+                break;
+        }
+    }
+
+    void ExibirExtrato()
+    {
+        Console.WriteLine("*************Etrato**************");
+        foreach (Movimentacao nov in cliente.Movimentacoes)
+        {
+            Console.WriteLine($"{nov.Tipo} - {nov.Valor}");
+        }
+        Console.WriteLine("Deseja exibir o menu novamente? Digite S ou N");
+        string exibirMenu = Console.ReadLine();
+        if( exibirMenu == "S" ) 
+        {
+            ExibirMenu(cliente);
+        }
+        else
+        {
+            Console.WriteLine("Deseja consultar outro cliente? Digite S ou N");
+            string consultarCliente = Console.ReadLine();
+            if (consultarCliente == "S")
+                ConsultarCliente();   
+        }
+    }
+
+    void RealizarSaque(Cliente cliente)
+    {
+        Console.WriteLine("Digite o valor que deseja sacar:");
+        string valor = Console.ReadLine();
+        cliente.RealizarSaque(Convert.ToDecimal(valor));
+        Console.WriteLine("Deseja Realizar outra transacao? Digite Sou N");
+        string realizarOutraTransacao = Console.ReadLine();
+        if (realizarOutraTransacao == "S")
+            ExibirMenu(cliente);
+        else
+            Console.WriteLine("Foi um prazer lhe atender! Ate mais!");
+
+    }
+
+    void RealizarDeposito(Cliente cliente)
+    {
+        Console.WriteLine("Digite o valor que deseja depositar:");
+        string valor = Console.ReadLine();
+        cliente.RealizarDeposito(Convert.ToDecimal(valor));
+        Console.WriteLine("Deseja Realizar outra transacao? Digite Sou N");
+        string realizarOutraTransacao = Console.ReadLine();
+        if (realizarOutraTransacao == "S")
+            ExibirMenu(cliente);
+        else
+            Console.WriteLine("Foi um prazer lhe atender! Ate mais!");
+
+
+    }
+}
